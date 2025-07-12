7013e4ab8efefb0e1068ead1cbf3478352ec4458
Cristian Yulma <cristianyulmaw@gmail.com>
2025-07-12 12:45:38 +0700

    Feat: add buatrentang features

---
+++ tasks/Cristian/no6.js
@@ -0,0 +1,12 @@
+function buatrentang(mulai, selesai){
+    if (typeof mulai !== 'number' || typeof selesai !== 'number' || mulai > selesai){
+        console.log("Parameter harus bertipe data number dan positif")
+        return ;
+    }
+    let hasil = []
+    for (let i = mulai; i <= selesai; i++){
+        hasil.push(i)
+    }
+    console.log(hasil.join(", "))
+}
+module.exports=buatrentang
