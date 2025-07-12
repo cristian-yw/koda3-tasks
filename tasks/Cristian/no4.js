04f4f831d3adf500836172bb34ee73f863f1231c
Cristian Yulma <cristianyulmaw@gmail.com>
2025-07-12 12:43:51 +0700

    Feat: add Process Number features

---
+++ tasks/Cristian/no4.js
@@ -0,0 +1,17 @@
+function countarray2(arr){
+    if(!Array.isArray(arr)){
+        return "Parameter harus berupa array "
+    }
+    let hasil = 1
+    let adaganjil = false
+    for(let i = 0; i < arr.length;i++){
+        if (arr[i] % 2 !== 0){
+            hasil *=arr[i]
+            adaganjil=true
+        }
+    }
+    console.log(hasil)
+    return adaganjil ? hasil : 1;
+   
+}
+module.exports = countarray2;
