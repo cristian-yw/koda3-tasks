b9025dc05508885307b666e83cf2dfa5f27ba79e
Cristian Yulma <cristianyulmaw@gmail.com>
2025-07-12 12:41:38 +0700

    Feat: add Count Array features

---
+++ tasks/Cristian/no2.js
@@ -0,0 +1,14 @@
+function countarray(arr){
+    if(!Array.isArray(arr)){
+        return "Parameter harus berupa array "
+    }
+    let hasil = 1
+    for(let i = 0; i < arr.length;i++){
+        if (typeof arr[i] !== "number"){
+            return "Parameter harus number"
+        }
+        hasil *=arr[i]
+    }
+    console.log(hasil)
+}
+module.exports = countarray;
